f1eb98f7597a5adc0497943aa8616a7f84586498
Jeremy Malana <malanam.136518130255@depedqc.ph>
2025-07-05 08:37:38 +0800

    Responsive

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-  const toggleBtn = document.getElementById('theme-toggle');
-  const icon = document.getElementById('theme-icon');
+const toggleBtn = document.getElementById('theme-toggle');
+const icon = document.getElementById('theme-icon');
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-theme');
+  icon.classList.remove('ri-moon-line');
+  icon.classList.add('ri-sun-line');
+}
+toggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.body.classList.toggle('dark-theme');
+  
+  const isDark = document.body.classList.contains('dark-theme');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  icon.classList.toggle('ri-moon-line');
+  icon.classList.toggle('ri-sun-line');
+});
 
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark-theme');
-    icon.classList.remove('ri-moon-line');
-    icon.classList.add('ri-sun-line');
-  }
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
 
-  toggleBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.body.classList.toggle('dark-theme');
-    
-    const isDark = document.body.classList.contains('dark-theme');
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 
-    icon.classList.toggle('ri-moon-line');
-    icon.classList.toggle('ri-sun-line');
+
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
   });
+});
